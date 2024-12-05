fe05d9a4517a979b51102b46ccfd12cbb2024c36
Izak Jackson <me@izakjackson.com>
2024-12-05 18:11:50 +0000

    🛠️ Added Nuxt Test Utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/test-utils/module'
+  ],
   css: ['~/assets/css/main.css'],
 });
